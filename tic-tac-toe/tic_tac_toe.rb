d6c9277f7a0b5d55dcf2747d30df093520bbd154
Brady Swenson <bradyswenson@gmail.com>
2014-12-06 23:03:39 -0600

    board_stats > board

--- tic-tac-toe/tic_tac_toe.rb
+++ tic-tac-toe/tic_tac_toe.rb
@@ -2,60 +2,60 @@
 
 require 'pry'
 
-board_status = Hash.new
+board = Hash.new
 winning_combinations = [[1,2,3], [1,5,9], [1,4,7], [2,5,8], [3,5,7], [4,5,6], [7,8,9], [3,6,9]] 
 score = {player: 0, computer: 0, tie: 0}
 
-def draw_board(bs) #board_status
+def draw_board(b) #board
   puts "
              |     |
-          #{bs[1]}  |  #{bs[2]}  |  #{bs[3]}  
+          #{b[1]}  |  #{b[2]}  |  #{b[3]}  
         _____|_____|_____
              |     |
-          #{bs[4]}  |  #{bs[5]}  |  #{bs[6]}  
+          #{b[4]}  |  #{b[5]}  |  #{b[6]}  
         _____|_____|_____
              |     |
-          #{bs[7]}  |  #{bs[8]}  |  #{bs[9]}
+          #{b[7]}  |  #{b[8]}  |  #{b[9]}
              |     |
 "
 end
 
-def remaining_options(bs) #board_status
-  bs.keys.select {|position| bs[position] == ' '}
+def remaining_options(b) #board
+  b.keys.select {|position| b[position] == ' '}
 end
 
-def get_computer_choice(bs, wc) #board_status, remaining_options, winning_combinations
+def get_computer_choice(b, wc) #board, winning_combinations
    wc.each do |combo|
-    if bs[combo[0]] == 'O' and bs[combo[1]] == 'O' and bs[combo[2]] == ' '
+    if b[combo[0]] == 'O' and b[combo[1]] == 'O' and b[combo[2]] == ' '
         return combo[2]
-    elsif bs[combo[1]] == 'O' and bs[combo[2]] == 'O' and bs[combo[0]] == ' '
+    elsif b[combo[1]] == 'O' and b[combo[2]] == 'O' and b[combo[0]] == ' '
         return combo[0]
-    elsif bs[combo[0]] == 'O' and bs[combo[2]] == 'O' and bs[combo[1]] == ' '
+    elsif b[combo[0]] == 'O' and b[combo[2]] == 'O' and b[combo[1]] == ' '
         return combo[1]
-    elsif bs[combo[0]] == 'X' and bs[combo[1]] == 'X' and bs[combo[2]] == ' '
+    elsif b[combo[0]] == 'X' and b[combo[1]] == 'X' and b[combo[2]] == ' '
         return combo[2]
-    elsif bs[combo[1]] == 'X' and bs[combo[2]] == 'X' and bs[combo[0]] == ' '
+    elsif b[combo[1]] == 'X' and b[combo[2]] == 'X' and b[combo[0]] == ' '
         return combo[0]
-    elsif bs[combo[0]] == 'X' and bs[combo[2]] == 'X' and bs[combo[1]] == ' '
+    elsif b[combo[0]] == 'X' and b[combo[2]] == 'X' and b[combo[1]] == ' '
         return combo[1]
     end
   end
-  if bs[5] == ' '
+  if b[5] == ' '
     return 5
   else
-    return remaining_options(bs).sample
+    return remaining_options(b).sample
   end
 end
 
-def check_winner(bs, wc, score) #board_status, winning_combinations, score
+def check_winner(b, wc, score) #board, winning_combinations, score
   wc.each do |combo|
-    if bs[combo[0]] == 'X' and bs[combo[1]] == 'X' and bs[combo[2]] == 'X'
+    if b[combo[0]] == 'X' and b[combo[1]] == 'X' and b[combo[2]] == 'X'
       score[:player] += 1
       return 'Player'
-    elsif bs[combo[0]] == 'O' and bs[combo[1]] == 'O' and bs[combo[2]] == 'O'
+    elsif b[combo[0]] == 'O' and b[combo[1]] == 'O' and b[combo[2]] == 'O'
       score[:computer] += 1
       return 'Computer' 
-    elsif remaining_options(bs) == []
+    elsif remaining_options(b) == []
       score[:tie] += 1
       return 'Tie'
     end
@@ -66,12 +66,12 @@ end
 begin #continue loop
 
   #initialize game data
-  board_status = {1 => ' ', 2 =>  ' ', 3 => ' ', 4 => ' ', 5 => ' ', 6 => ' ', 7 => ' ', 8 => ' ', 9 => ' '}
+  board = {1 => ' ', 2 =>  ' ', 3 => ' ', 4 => ' ', 5 => ' ', 6 => ' ', 7 => ' ', 8 => ' ', 9 => ' '}
   remaining_options = Array(1..9)
 
   begin #game loop
 
-    draw_board(board_status)
+    draw_board(board)
     
     #get player move
     begin 
@@ -80,25 +80,25 @@ begin #continue loop
     end until remaining_options.include?(player_choice)
 
     #track player move
-    board_status[player_choice] = 'X'
+    board[player_choice] = 'X'
     remaining_options.delete(player_choice)
 
     #check for winner
-    winner = check_winner(board_status, winning_combinations, score)
+    winner = check_winner(board, winning_combinations, score)
 
     #if no winner, get computer move
     if winner == nil 
-      comp_choice = get_computer_choice(board_status, winning_combinations)
+      comp_choice = get_computer_choice(board, winning_combinations)
       #track computer move
-      board_status[comp_choice] = 'O'
+      board[comp_choice] = 'O'
       remaining_options.delete(comp_choice)
       #update winner check
-      winner = check_winner(board_status, winning_combinations, score)
+      winner = check_winner(board, winning_combinations, score)
     end
 
   end until winner == 'Player' || winner == 'Computer' || winner == 'Tie'
 
-  draw_board(board_status)
+  draw_board(board)
 
   if winner == 'Player' 
     puts "You won!"

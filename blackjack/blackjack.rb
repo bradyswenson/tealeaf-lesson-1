a8599ecbde457603f70e22ad501fe5aa736cc5ef
Brady Swenson <bradyswenson@gmail.com>
2014-12-08 07:39:26 -0600

    hand total done, other method refactors

--- blackjack/blackjack.rb
+++ blackjack/blackjack.rb
@@ -18,12 +18,6 @@ def deal_card(deck) #Integer - number of cards, deck
 	card = deck.shift
 end
 
-def draw_table(deck, ph, dh) #deck, player_hand, dealer_hand
-	system 'clear'
-	draw_hand(ph)
-	draw_hand(dh)
-end
-
 def draw_hand(hand)
 	line1 = ""
 	line2 = ""
@@ -48,23 +42,95 @@ def draw_hand(hand)
 	puts line4
 end
 
+def draw_table(deck, player_hand, dealer_hand) 
+	system 'clear'
+	draw_hand(dealer_hand)
+	draw_hand(player_hand)
+	binding.pry
+	puts "Player has #{get_hand_total(player_hand)[0]}. Dealer has #{get_hand_total(dealer_hand)[0]}."
+end
+
 def start_player_hand(deck, player_hand)
 	player_hand.push(deal_card(deck))
 	player_hand.push(deal_card(deck))
 end
 
-def start_dealer_hand(deck, dealer_hand) #deck, dealer_hand
+def start_dealer_hand(deck, dealer_hand) 
 	dealer_hand.push(deal_card(deck))
 	dealer_hand.push(deal_card(deck))
 end
 
-deck = initialize_deck(2, deck)
-deck = deck.shuffle
+def check_winner(dealer_hand, player_hand)
+	return 'Player'
+end 
 
-player_hand = start_player_hand(deck, player_hand)
-dealer_hand = start_dealer_hand(deck, dealer_hand)
+def get_hand_total(hand)
+	total = [0, 0]
+	hand.each do |card|
+		if card[2].kind_of?(Array)
+			total[0] += card[2][0]
+			total[1] += card[2][1]
+		else
+			total[0] += card[2]
+			total[1] += card[2]
+		end
+	end
+	return total
+end
 
-draw_table(deck, player_hand, dealer_hand)
+def check_bust(hand)
+	if get_hand_total(hand)[0] > 21
+		return true
+	end
+	return false
+end
+
+begin #continue loop
+
+	deck = initialize_deck(2, deck)
+	deck = deck.shuffle
+
+	player_hand = []
+	dealer_hand = []
+
+	player_hand = start_player_hand(deck, player_hand)
+	dealer_hand = start_dealer_hand(deck, dealer_hand)
+
+	begin #game loop
+
+		draw_table(deck, player_hand, dealer_hand)
+
+		begin #player loop
+
+			begin 
+	      puts "Stay or Hit? (S/H)"
+	      choice = gets.chomp
+    	end until choice.downcase == 's' || choice.downcase == 'h'
+
+    	if choice == 'h'
+    		player_hand.push(deal_card(deck))
+    		draw_table(deck, player_hand, dealer_hand)
+    	end
+
+		end until choice == 's' || check_bust(player_hand) == true
+
+		if check_bust(player_hand) == true
+			puts "You busted."
+			break
+		end
+
+
+
+		winner = check_winner(dealer_hand, player_hand)	
+
+	end until winner == 'Player' || winner == 'Dealer' || winner == 'Push'
+
+
+  puts "Do you want to play again? (Y/N)"
+  continue = gets.chomp
+
+end while continue.downcase == 'y'
+
 
 #binding.pry 
 
